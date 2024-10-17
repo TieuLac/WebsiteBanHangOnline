9af6fd8b0c5cb05a111afc50ac11ba96bbf85701
TieuLac <nlac283@gmail.com>
2024-10-18 01:59:32 +0700

    hoan thanh chuc nang xem, them, sua, xoa danh muc san pham

--- WebBanHangOnline/Areas/Admin/Controllers/ProductsController.cs
+++ WebBanHangOnline/Areas/Admin/Controllers/ProductsController.cs
@@ -30,6 +30,7 @@ namespace WebBanHangOnline.Areas.Admin.Controllers
 
         public ActionResult Add()
         {
+            ViewBag.ProductCategory = new SelectList(db.ProductCategories.ToList(),"Id","Title");
             return View();
         }
     }
